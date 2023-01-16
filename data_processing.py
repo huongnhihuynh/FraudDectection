3f1ffb7c3afe2a03d8c40173dcc162e499356b4e
huongnhihuynh <huongnhihuynh@gmail.com>
2023-01-16 15:35:29 +0700

    correct the code for sum function & implemented new multiplication function

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,17 @@
 
 print('adding all the data processing code related frau detection')
+
+
+a=5
+b=7
+def sum(a,b):
+    c=a+b
+    return c
+
+print('sum function')
+
+def multiplication(a,b):
+    d=a*b
+    return d 
+
+print('mutiplication function')
